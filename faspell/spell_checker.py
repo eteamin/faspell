839c6994ecbe42b9e7c368990dc472e48419dda1
Amin <eteamin@yahoo.com>
2016-06-03 14:13:01 +0430

    optimized faspell

--- faspell/spell_checker.py
+++ faspell/spell_checker.py
@@ -6,38 +6,42 @@ import re
 class SpellChecker(object):
 
     def __init__(self, database):
-        self.NWORDS = database
-    alphabet = 'آ ا ب پ ت ث ج چ ح خ د ذ ر ز ژ س ش ص ض ط ظ ع غ ف ق ک گ ل م ن و ه ی'
+        self.all_words = database
+        self. alphabet = 'آ ا ب پ ت ث ج چ ح خ د ذ ر ز ژ س ش ص ض ط ظ ع غ ف ق ک گ ل م ن و ه ی'
 
-    def first_edit(self, word):
-        s = [(word[:i], word[i:]) for i in range(len(word) + 1)]
-        deletes = [a + b[1:] for a, b in s if b]
-        transposes = [a + b[1] + b[0] + b[2:] for a, b in s if len(b) > 1]
-        replaces = [a + c + b[1:] for a, b in s for c in self.alphabet if b]
-        inserts = [a + c + b for a, b in s for c in self.alphabet]
+    def edit(self, word):
+        splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+        deletes = [a + b[1:] for a, b in splits if b]
+        transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b) > 1]
+        replaces = [a + c + b[1:] for a, b in splits for c in self.alphabet if b]
+        inserts = [a + c + b for a, b in splits for c in self.alphabet]
         return set(deletes + transposes + replaces + inserts)
 
-    def known(self, words): return set(w for w in words if w in self.NWORDS)
-
-    def normalize(self, word):
-        if '؟' in word:
-            normalized = word.replace('؟', '')
-            return normalized
-        return word
+    def declare_known_words(self, words):
+        return set(w for w in words if w in self.all_words)
 
     def correct(self, word):
         normalized = self.normalize(word)
-        candidates = list(self.known([normalized])) or list(self.known(self.first_edit((normalized))))
+        candidates = list(self.declare_known_words([normalized])) or list(self.declare_known_words(self.edit(normalized)))
+        return self.analyze_correction(candidates, normalized, word)
 
-        if candidates == []:
-            return self.dictionary_maker(normalized, candidates)
-        elif candidates[0] == normalized and len(candidates) == 1:
+        # this analysis is logically based on CKEditor response expectation:
+    def analyze_correction(self, candidates, normalized, word):
+        if candidates == []:  # no result due to non-persian input
+            return self.make_dictionary(normalized, candidates)
+        elif candidates[0] == normalized and len(candidates) == 1:  # candidate is same as word (word is correct)
             return []
-        else:
-            return self.dictionary_maker(word, candidates)
+        else:  # word is misspelled, ergo returning the word and the suggested candidates
+            return self.make_dictionary(word, candidates)
 
     @staticmethod
-    def dictionary_maker(word, candidates):
+    def normalize(word):
+        if '؟' in word:
+            return word.replace('؟', '')
+        return word
+
+    @staticmethod
+    def make_dictionary(word, candidates):
         suggestion = []
         for item in candidates:
             suggestion.append(item)
@@ -61,6 +65,6 @@ if __name__ == '__main__':
 
     with open('dictionary', 'r') as my_dictionary:
         check_spelling = SpellChecker(train(words(my_dictionary.read())))
-        print(check_spelling.correct(''))
+        print(check_spelling.correct('%$#%$#^#'))
 
 
